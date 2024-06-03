55dc86e5dbadd9489e654a404cc0cd5d73ed0ca4
Usuario <Usuario@CPD03-M19>
2024-06-03 12:29:43 -0300

    começando a data services

--- BuscaCep/Models/Logradouro.cs
+++ BuscaCep/Models/Logradouro.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace BuscaCep.Models
 {
-    internal class Logradouro
+    public class Logradouro
     {
+        public string CEP { get; set; }
+        public string id_logradouro { get; set; }
+        public string tipo { get; set; }
+        public string descrcao { get; set; }
+        public string id_cidade { get; set; }
+        public string UF { get; set; }
+        public string complemento { get; set; }
+        public string descrticao_sem_numero { get; set; }
+        public string descricao_cidade { get; set; }
+        public string codigo_cidade_ibge { get; set; }
+        public string descricao_bairro { get; set; }
     }
 }
---
+++ BuscaCep/Services/DataServices.cs
@@ -0,0 +1,22 @@
+﻿using BuscaCep.Models;
+using Newtonsoft.Json;
+namespace BuscaCep.Services
+{
+    public class DataServices
+    {
+        public static async Task<Endereco> GetEnderecoAsync (string cep)
+        {
+            Endereco end;
+
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "Http://cep.metoda.com.br/endereco/by-cep?cep=" + cep;
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+    }
+}
